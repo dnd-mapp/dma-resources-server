1b7dcfa2eb4e0afd30316727c5b7bb2dec1f0894
NoNamer777 <oscar.wellner@gmail.com>
2025-10-11 11:25:40 +0200

    Run Prettier

--- src/app/app.controller.spec.ts
+++ src/app/app.controller.spec.ts
@@ -3,18 +3,18 @@ import { AppController } from './app.controller';
 import { AppModule } from './app.module';
 
 describe('AppController', () => {
-  async function setupTest() {
-    const app = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    async function setupTest() {
+        const app = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
 
-    return {
-      controller: app.get(AppController),
-    };
-  }
+        return {
+            controller: app.get(AppController),
+        };
+    }
 
-  it('should return "Hello World!"', async () => {
-    const { controller } = await setupTest();
-    expect(controller.getHello()).toBe('Hello World!');
-  });
+    it('should return "Hello World!"', async () => {
+        const { controller } = await setupTest();
+        expect(controller.getHello()).toBe('Hello World!');
+    });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { AppModule } from './app';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const app = await NestFactory.create(AppModule, new FastifyAdapter());
 
-  await app.listen(process.env.PORT ?? 3000);
-}
-
-(async () => await bootstrap())();
+await app.listen(process.env['PORT'] ?? 3000);
