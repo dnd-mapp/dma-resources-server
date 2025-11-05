1d548c08d4bc89f45415876ef5300b32bb531493
NoNamer777 <oscar.wellner@gmail.com>
2025-11-05 17:13:38 +0100

    Log when the server is available

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { AppModule } from './app';
 
 const app = await NestFactory.create(AppModule, new FastifyAdapter());
 
-await app.listen(process.env['PORT'] ?? 3000);
+const port = process.env['DMA_PORT'] ?? 3000;
+const host = process.env['DMA_HOST'] ?? 'localhost';
 
 app.setGlobalPrefix('server');
 
 app.enableShutdownHooks();
+
+await app.listen(port, host, () => {
+    Logger.log(`The D&D Mapp Resources server is available on: http://${host}:${port}/server`, 'NestApplication');
+});
