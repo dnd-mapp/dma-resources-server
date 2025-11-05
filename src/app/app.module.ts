548c5d0d21ab7772fdb7b88a1ebe987d902fb36b
NoNamer777 <oscar.wellner@gmail.com>
2025-11-05 17:16:52 +0100

    Remove default app controller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { HealthModule } from './health';
 
 @Module({
-    controllers: [AppController],
     imports: [HealthModule],
 })
 export class AppModule {}
