83ac95a5b69399574719381f8222324e460d6692
NoNamer777 <oscar.wellner@gmail.com>
2025-11-05 17:13:16 +0100

    Set global prefix

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,6 @@ const app = await NestFactory.create(AppModule, new FastifyAdapter());
 
 await app.listen(process.env['PORT'] ?? 3000);
 
+app.setGlobalPrefix('server');
+
 app.enableShutdownHooks();
