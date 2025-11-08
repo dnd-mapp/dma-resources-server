2bd5292358d58134d38528932cdef15c3ef7b15c
NoNamer777 <oscar.wellner@gmail.com>
2025-11-08 15:34:41 +0100

    Add MARIADB_SHADOW_URL environment variable

--- prisma.config.ts
+++ prisma.config.ts
@@ -11,5 +11,6 @@ export default defineConfig({
     engine: 'classic',
     datasource: {
         url: process.env['MARIADB_URL'],
+        shadowDatabaseUrl: process.env['MARIADB_SHADOW_URL'],
     },
 });
