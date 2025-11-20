d137125a8d7c6c255b54269736244be9671dbe0b
NoNamer777 <oscar.wellner@gmail.com>
2025-11-20 20:21:52 +0100

    Adjust tests to used typed responses

--- src/app/health/health.controller.spec.ts
+++ src/app/health/health.controller.spec.ts
@@ -2,6 +2,7 @@ import { createTestEnvironment, withMockedQuery } from '@dnd-mapp/dma-resources-
 import { ServiceUnavailableException } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { HealthModule } from './health.module';
+import { HealthStatuses, RunningStatuses, StatusDto } from './status.dto';
 
 describe('HealthController', () => {
     interface SetupTestParams {
@@ -27,20 +28,22 @@ describe('HealthController', () => {
     it('should return live check', async () => {
         const { controller } = await setupTest();
 
-        expect(controller.live()).toEqual({ status: 'ok' });
+        expect(controller.live()).toEqual(new StatusDto(HealthStatuses.OK));
     });
 
     it('should return ready check', async () => {
         const { controller } = await setupTest();
+        const status = new StatusDto(HealthStatuses.OK);
+        status.setCheck('database', RunningStatuses.UP);
 
-        expect(await controller.ready()).toEqual({ status: 'ok', checks: { database: 'up' } });
+        expect(await controller.ready()).toEqual(status);
     });
 
     it('should return ready check when database is unavailable', async () => {
         const { controller } = await setupTest({ databaseAvailable: false });
+        const status = new StatusDto(HealthStatuses.OK);
+        status.setCheck('database', RunningStatuses.DOWN, 'Reason why database is down');
 
-        await expect(controller.ready()).rejects.toEqual(
-            new ServiceUnavailableException({ status: 'unhealthy', checks: { database: 'down' } }),
-        );
+        await expect(controller.ready()).rejects.toEqual(new ServiceUnavailableException(status));
     });
 });
