7a1ecfec124e5f007078b4e6ab389efbcd753dc9
NoNamer777 <oscar.wellner@gmail.com>
2025-11-20 17:57:00 +0100

    Add ready end-point
    - Add proper response typing
    - Include database check in read end-point

--- src/app/health/health.controller.spec.ts
+++ src/app/health/health.controller.spec.ts
@@ -1,20 +1,46 @@
-import { Test } from '@nestjs/testing';
+import { createTestEnvironment, withMockedQuery } from '@dnd-mapp/dma-resources-server/test';
+import { ServiceUnavailableException } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { HealthModule } from './health.module';
 
 describe('HealthController', () => {
-    async function setupTest() {
-        const app = await Test.createTestingModule({
+    interface SetupTestParams {
+        databaseAvailable: boolean;
+    }
+
+    async function setupTest(params: SetupTestParams = { databaseAvailable: true }) {
+        if (params.databaseAvailable) {
+            withMockedQuery('SELECT 1;', () => 1);
+        } else {
+            withMockedQuery('SELECT 1;', () => new Error('Database unavailable'));
+        }
+
+        const app = await createTestEnvironment({
             imports: [HealthModule],
-        }).compile();
+        });
 
         return {
             controller: app.get(HealthController),
         };
     }
 
-    it('should return health check', async () => {
+    it('should return live check', async () => {
         const { controller } = await setupTest();
-        expect(controller.check()).toEqual({ message: 'Healthy' });
+
+        expect(controller.live()).toEqual({ status: 'ok' });
+    });
+
+    it('should return ready check', async () => {
+        const { controller } = await setupTest();
+
+        expect(await controller.ready()).toEqual({ status: 'ok', checks: { database: 'up' } });
+    });
+
+    it('should return ready check when database is unavailable', async () => {
+        const { controller } = await setupTest({ databaseAvailable: false });
+
+        await expect(controller.ready()).rejects.toEqual(
+            new ServiceUnavailableException({ status: 'unhealthy', checks: { database: 'down' } }),
+        );
     });
 });
